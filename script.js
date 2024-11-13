fcc56a2e205fecb8621b6bf43011ff60e8c6cab5
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-13 00:14:06 -0800

    function that accepts a number from 0-100

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ let myGrades = [100, 100, 90, 73, 78, 94, 86]; let myAverage = 0;
 myAverage = 0;
 
 /*The loop is incorrectly starting at 1 instead of 0, which misses the first element and causes an out-of-bounds error on the last iteration. */
+/*Changed loop to start from 0 and run until i < myGrades.length to correctly sum all elements.*/
 for (let i = 0; i < myGrades.length; i++) { myAverage += myGrades[i]; }
 
     /* The loop condition i <= myGrades.length should be i < myGrades.length to prevent accessing an undefined index. */
-
 myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
+
+/* function that accepts a number from 0-100 */
+function validateGrade(value) {
+    if (typeof value !== 'number') {
+        throw new Error('Input must be a number');
+    }
+    if (value < 0) {
+        throw new Error('Input must be greater than or equal to 0');
+    }
+    if (value > 100) {
+        throw new Error('Input must be less than or equal to 100');
+    }
+    return value;
+}

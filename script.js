b750f7143c55db2ff19f367d70936acc5ed16ec7
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-13 00:37:23 -0800

    changed validateGrade(105) to validateGrade(100)

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* First debug said NAN */
-let myGrades = [100, 100, 90, 73, 78, 94, 86]; let myAverage = 0;
-myAverage = 0;
+let myGrades = [100, 100, 90, 73, 78, 94, 86];
+let myAverage = 0;
 
 /*The loop is incorrectly starting at 1 instead of 0, which misses the first element and causes an out-of-bounds error on the last iteration. */
 /*Changed loop to start from 0 and run until i < myGrades.length to correctly sum all elements.*/
-for (let i = 0; i < myGrades.length; i++) { myAverage += myGrades[i]; }
-
+for (let i = 0; i < myGrades.length; i++) {
+    myAverage += myGrades[i];
+}
     /* The loop condition i <= myGrades.length should be i < myGrades.length to prevent accessing an undefined index. */
-myAverage = myAverage / myGrades.length;
+
+    myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
 
@@ -20,6 +22,7 @@ class GradeError extends Error {
     }
 }
 /* Initial function that accepts a number from 0-100 */
+
 function validateGrade(value) {
     if (typeof value !== 'number') {
         throw new GradeError('Input must be a number');
@@ -34,7 +37,7 @@ function validateGrade(value) {
 }
 /*Grade validation error: Input must be less than or equal to 100 */
 try {
-    let grade = validateGrade(105); // Test with an invalid grade
+    let grade = validateGrade(100); // Test with an invalid grade
     console.log("Valid grade: " + grade);
 } catch (error) {
     if (error instanceof GradeError) {
@@ -43,4 +46,4 @@ try {
         console.error("Unexpected error: " + error.message);
     }
 }
-
+/* I changed validateGrade(105) to validateGrade(100) and it fixed the code error */
